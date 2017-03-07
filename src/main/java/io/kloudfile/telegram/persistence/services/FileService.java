2d918087db1eb69a333b338995a3690adfc4be18
probE466 <register@probE466.me>
2017-03-07 22:00:54 +0100

    completed fileservice

--- src/main/java/io/kloudfile/telegram/persistence/services/FileService.java
+++ src/main/java/io/kloudfile/telegram/persistence/services/FileService.java
@@ -13,24 +13,44 @@ public class FileService {
 
     private final File chatIdFile = new File("chatIds");
     private final Logger logger = Logger.getLogger(this.getClass());
+    private Set<Integer> chatIdSet = new HashSet<>();
+
+    public Set<Integer> getChatIdSet() {
+        loadFile();
+        return chatIdSet;
+    }
 
     private void loadFile() {
-        if(!chatIdFile.isFile()) {
+        createFile();
+
+        if(this.chatIdSet.isEmpty()) {
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(chatIdFile))) {
+                this.chatIdSet = (HashSet<Integer>) objectInputStream.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void syncFile() {
+        createFile();
+
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(chatIdFile))) {
+            out.writeObject(chatIdSet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createFile() {
+        if (!chatIdFile.exists()) {
             try {
-                if (chatIdFile.createNewFile()) {
+                if (!chatIdFile.createNewFile()) {
                     logger.log(Level.FATAL, "Could not save file, check writepriveledges");
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-
-        HashSet<Integer> chatIdSet = null;
-
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(chatIdFile))) {
-            chatIdSet = (HashSet<Integer>) objectInputStream.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
         }
     }
 }
