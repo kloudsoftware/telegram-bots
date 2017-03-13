d4cc9ca4e574219d9e3f8b21556e301ae5becdc2
Fred Lahde <fredlahde@gmail.com>
2017-03-13 12:26:04 +0100

    update DB Schema

--- database/telegram.sql
+++ database/telegram.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `telegramdb`.`user` (
   `firstname` VARCHAR(45) NULL,
   `lastname` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -37,11 +37,44 @@ CREATE TABLE IF NOT EXISTS `telegramdb`.`keyword` (
   PRIMARY KEY (`id`),
   INDEX `fk_keywords_users_idx` (`user_id` ASC),
   CONSTRAINT `fk_keywords_users`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `telegramdb`.`user` (`id`)
+  FOREIGN KEY (`user_id`)
+  REFERENCES `telegramdb`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `telegramdb`.`subject_area`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `telegramdb`.`subject_area` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `hostkey` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+  ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `telegramdb`.`user_subject_area`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `telegramdb`.`user_subject_area` (
+  `user_id` INT NOT NULL,
+  `subject_area_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `subject_area_id`),
+  INDEX `fk_user_has_subject_area_subject_area1_idx` (`subject_area_id` ASC),
+  INDEX `fk_user_has_subject_area_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_subject_area_user1`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `telegramdb`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_subject_area_subject_area1`
+  FOREIGN KEY (`subject_area_id`)
+  REFERENCES `telegramdb`.`subject_area` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
