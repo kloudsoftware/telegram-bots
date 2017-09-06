2714711973e2752735ed3aa3523ff9637dfc80fc
Fred Lahde <fredlahde@gmail.com>
2017-09-06 09:42:42 +0200

    fix deps

--- src/main/java/io/kloudfile/telegram/InfosysBotApplication.java
+++ src/main/java/io/kloudfile/telegram/InfosysBotApplication.java
@@ -1,16 +1,24 @@
 package io.kloudfile.telegram;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
 
 
 @SpringBootApplication
 @EnableScheduling
 public class InfosysBotApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Properties props = new Properties();
+		props.load(InfosysBotApplication.class.getResourceAsStream("/log4j.properties"));
+		PropertyConfigurator.configure(props);
+
 		Configuration cfg = new Configuration();
 		cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
 
